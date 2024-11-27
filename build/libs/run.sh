4e59d355c7c740a6a2670031a59cf50a9a5ad3c0
hyzr <hayderabbassi5@gmail.com>
2024-11-27 19:50:58 +0000

    wants to wrapp

---
+++ build/libs/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java --module-path /lib/jvm/javafx-sdk-23.0.1/lib --add-modules javafx.controls,javafx.fxml -jar /home/hyzr/documents/dev/java/ecole_primare/build/libs/ecole_primare-1.0.0-all.jar
